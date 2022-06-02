b99109ae57b66894f8a950a3fb7d249868fafb21
Igor Aleksandrov <ivaleksandrov@avito.ru>
2022-06-02 14:50:35 +0300

    release interface

--- circle.go
+++ circle.go
@@ -1,6 +1,17 @@
 package golang_united_school_homework
 
+import "math"
+
 // Circle must satisfy to Shape interface
 type Circle struct {
 	Radius float64
+}
+
+func (c *Circle) CalcPerimeter() float64 {
+	return 2 * c.Radius * math.Pi
+}
+
+// CalcArea returns calculation result of area
+func (c *Circle) CalcArea() float64 {
+	return c.Radius * c.Radius * math.Pi
 }
